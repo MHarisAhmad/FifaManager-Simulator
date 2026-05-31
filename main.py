acabe6f691abd16ae3b4c9593384074b4e9f46a9
MHarisAhmad <muhammadharisahmad82@gmail.com>
2026-05-31 23:12:20 +0500

    feat: 02_Addded simple Budget System

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ print("WELCOME TO THE FIFA MANAGER SIMULATOR")
 print("====================================")
 
 # Asking the user for their name and club
-manager_name = input("Enter your name, Manager: ")
-club_name = input("What club are you taking over? ")
+manager_name = input("Enter your name, Manager: ").title()
+club_name = input("What club are you taking over? ").title()
 
-print(f"\nBreaking News: {manager_name} has just been announced as the new manager of {club_name}!")
+# --- STEP 2: THE BUDGET SYSTEM ---
+budget = 150000000 
+
+print("\n====================================")
+print(f"Breaking News: {manager_name} has just been announced as the new manager of {club_name}!")
+print(f"Board Expectation: You have been given a transfer budget of €{budget:,}")
+print("====================================")
