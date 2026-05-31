f5102083080bf7f12e936c63bd9203ebbebc6198
MHarisAhmad <muhammadharisahmad82@gmail.com>
2026-05-31 22:55:09 +0500

    feat: 01_Welcome screen added

---
+++ main.py
@@ -0,0 +1,10 @@
+# --- STEP 1: WELCOME AND SETUP ---
+print("====================================")
+print("WELCOME TO THE FIFA MANAGER SIMULATOR")
+print("====================================")
+
+# Asking the user for their name and club
+manager_name = input("Enter your name, Manager: ")
+club_name = input("What club are you taking over? ")
+
+print(f"\nBreaking News: {manager_name} has just been announced as the new manager of {club_name}!")
