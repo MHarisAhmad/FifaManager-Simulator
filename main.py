273a8c3086b2b40f0b160d7a029da30976ee2989
MHarisAhmad <muhammadharisahmad82@gmail.com>
2026-06-01 09:14:38 +0500

    feat: 05_Added Energy tracking attribute to players

--- main.py
+++ main.py
@@ -41,10 +41,10 @@ time.sleep(2)
 # --- STEP 4: YOUR STARTING SQUAD ---
 # A list containing 4 dictionaries (one for each player)
 squad = [
-    {"Name": "Starter Keeper", "Position": "GK", "OVR": 72},
-    {"Name": "Starter Defender", "Position": "CB", "OVR": 70},
-    {"Name": "Starter Midfielder", "Position": "CM", "OVR": 71},
-    {"Name": "Starter Striker", "Position": "ST", "OVR": 73}
+    {"Name": "Starter Keeper", "Position": "GK", "OVR": 72, "Energy": 100},
+    {"Name": "Starter Defender", "Position": "CB", "OVR": 70,"Energy": 100},
+    {"Name": "Starter Midfielder", "Position": "CM", "OVR": 71,"Energy": 100},
+    {"Name": "Starter Striker", "Position": "ST", "OVR": 73,"Energy": 100}
 ]
 
 print("\nLoading squad databases...")
@@ -53,5 +53,5 @@ time.sleep(1.5)
 print("\n=== YOUR CURRENT SQUAD ===")
 # We use a for loop to look at each individual player dictionary inside our squad list
 for player in squad:
-    print(f"[{player['Position']}] {player['Name']} - OVR: {player['OVR']}")
+    print(f"[{player['Position']}] {player['Name']} - OVR: {player['OVR']} | Energy: {player['Energy']}%")
 print("==========================")
